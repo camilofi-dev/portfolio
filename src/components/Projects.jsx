0ef82240042175b2ba4db4b17cf2ebd7ccc72317
Camilo Figueroa <cgfigueroa29@gmail.com>
2026-01-12 20:36:11 -0500

    redesign projects cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import todo from "../images/to-do.png";
+import portfolio from "../images/portfolio.png";
 
 export default function Projects() {
 	return (
@@ -7,30 +8,38 @@ export default function Projects() {
 				<span class="section-title">Projects</span>
 				<div class="project-list">
 					<div class="project-item">
-						<img src={todo} alt="to-do" width="40px" />
+						<img src={portfolio} alt="to-do" width="40px" />
+						<div class="labels">
+							<span class="label">React</span>
+							<span class="label">JavaScript</span>
+							<span class="label">CSS</span>
+							<span class="label">HTML</span>
+						</div>
 						<div class="projects-item-desc">
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
-							nihil at molestiae laudantium amet reiciendis id sit blanditiis,
-							velit ratione obcaecati? Laboriosam ex perspiciatis aut veritatis,
-							saepe reprehenderit natus. Cum.
+							<p>Portfolio</p>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Consequatur harum dicta error ad voluptas beatae fugiat corrupti
+								totam officia temporibus fugit quo cumque perspiciatis, quaerat
+								atque sit tenetur maiores doloribus.
+							</p>
 						</div>
 					</div>
 					<div class="project-item">
 						<img src={todo} alt="to-do" width="40px" />
-						<div class="projects-item-desc">
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
-							nihil at molestiae laudantium amet reiciendis id sit blanditiis,
-							velit ratione obcaecati? Laboriosam ex perspiciatis aut veritatis,
-							saepe reprehenderit natus. Cum.
+						<div class="labels">
+							<span class="label">JavaScript</span>
+							<span class="label">CSS</span>
+							<span class="label">HTML</span>
 						</div>
-					</div>
-					<div class="project-item">
-						<img src={todo} alt="to-do" width="40px" />
 						<div class="projects-item-desc">
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
-							nihil at molestiae laudantium amet reiciendis id sit blanditiis,
-							velit ratione obcaecati? Laboriosam ex perspiciatis aut veritatis,
-							saepe reprehenderit natus. Cum.
+							<p>To-do</p>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Consequatur harum dicta error ad voluptas beatae fugiat corrupti
+								totam officia temporibus fugit quo cumque perspiciatis, quaerat
+								atque sit tenetur maiores doloribus.
+							</p>
 						</div>
 					</div>
 				</div>
